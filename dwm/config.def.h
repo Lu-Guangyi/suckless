309bfd382ae12b1a84717d86139045b4f98ad2bb
Guangyi Lu <deluminnetsec@gmail.com>
2025-06-02 12:35:19 +0800

    Keybind for screenshotusing flameshot

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=13" };
+static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -62,6 +62,8 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *volup[] = { "amixer", "set", "Master", "5%+", NULL};
 static const char *voldown[] = { "amixer", "set", "Master", "5%-", NULL};
 static const char *voltog[] = { "amixer", "set", "Master", "toggle", NULL};
+static const char *flamegui[] = { "flame_gui.sh", NULL };
+static const char *flamescreen[] = { "flame_screen.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -91,6 +93,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_Right,  spawn,	   {.v = volup } },
 	{ MODKEY|ShiftMask,		XK_Left,   spawn,	   {.v = voldown } },
 	{ MODKEY|ShiftMask,		XK_Down,   spawn,	   {.v = voltog } },
+	{ MODKEY|ShiftMask,		XK_p,	   spawn,	   {.v = flamegui} },
+	{ MODKEY|ShiftMask,		XK_o,	   spawn,	   {.v = flamescreen} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
